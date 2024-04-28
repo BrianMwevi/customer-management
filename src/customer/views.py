1b254e30f0b6734ffbd8a0f89184877df4286371
Brian Mwevi <mwevibrian@gmail.com>
2024-04-28 20:33:24 +0300

    ft:add county, subcounty, ward and location views

--- src/customer/views.py
+++ src/customer/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
 
-# Create your views here.
+from src.customer.serializers import customer_serializer
+from src.customer.services.customer_service import CustomerService
+
+
+class CountyListCreateView(ListCreateAPIView):
+    queryset = CustomerService.filter_counties()
+    serializer_class = customer_serializer.CountySerializer
+
+
+class CountyRetrieveUpdateView(RetrieveUpdateAPIView):
+    queryset = CustomerService.filter_counties()
+    serializer_class = customer_serializer.CountySerializer
+
+
+class SubCountyListCreateView(ListCreateAPIView):
+    queryset = CustomerService.filter_counties()
+    serializer_class = customer_serializer.SubCountySerializer
+
+
+class SubCountyRetrieveUpdateView(RetrieveUpdateAPIView):
+    queryset = CustomerService.filter_counties()
+    serializer_class = customer_serializer.SubCountySerializer
+
+
+class WardListCreateView(ListCreateAPIView):
+    queryset = CustomerService.filter_counties()
+    serializer_class = customer_serializer.WardSerializer
+
+
+class WardRetrieveUpdateView(RetrieveUpdateAPIView):
+    queryset = CustomerService.filter_wards()
+    serializer_class = customer_serializer.WardSerializer
+
+
+class LocationListCreateView(ListCreateAPIView):
+    queryset = CustomerService.filter_counties()
+    serializer_class = customer_serializer.LocationSerializer
+
+
+class LocationRetrieveUpdateView(RetrieveUpdateAPIView):
+    queryset = CustomerService.filter_counties()
+    serializer_class = customer_serializer.LocationSerializer
+
