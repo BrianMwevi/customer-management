ce10e753970be887b79e8bf9ca75ee6027a1eb98
Brian Mwevi <mwevibrian@gmail.com>
2024-04-28 20:51:33 +0300

    ft:add business and customer views

--- src/customer/views.py
+++ src/customer/views.py
@@ -43,3 +43,32 @@ class LocationRetrieveUpdateView(RetrieveUpdateAPIView):
     queryset = CustomerService.filter_counties()
     serializer_class = customer_serializer.LocationSerializer
 
+
+class BusinessCategoryListCreateView(ListCreateAPIView):
+    queryset = CustomerService.filter_business_categories()
+    serializer_class = customer_serializer.BusinessCategorySerializer
+
+
+class BusinessCategoryRetrieveUpdateView(RetrieveUpdateAPIView):
+    queryset = CustomerService.filter_business_categories()
+    serializer_class = customer_serializer.BusinessCategorySerializer
+
+
+class BusinessListCreateView(ListCreateAPIView):
+    queryset = CustomerService.filter_businesses()
+    serializer_class = customer_serializer.BusinessSerializer
+
+
+class BusinessRetrieveUpdateView(RetrieveUpdateAPIView):
+    queryset = CustomerService.filter_businesses()
+    serializer_class = customer_serializer.BusinessSerializer
+
+
+class CustomerListCreateView(ListCreateAPIView):
+    queryset = CustomerService.filter_customers()
+    serializer_class = customer_serializer.CustomerSerializer
+
+
+class CustomerRetrieveUpdateView(RetrieveUpdateAPIView):
+    queryset = CustomerService.filter_customers()
+    serializer_class = customer_serializer.CustomerSerializer
