bc9f83daf2ff7ed2c402fc0c440c0b9d0dfb5923
Brian Mwevi <mwevibrian@gmail.com>
2024-04-28 19:38:34 +0300

    ft:add county, sub-county, ward,location, business category, business and customer models

--- src/customer/admin.py
+++ src/customer/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import County, SubCounty, Ward, Location, BusinessCategory, Business, Customer
+
+
+def get_model_fields(model):
+    fields = model._meta.fields
+    return [field.name for field in fields], [field.name for field in fields]
+
+
+@admin.register(County)
+class CountyAdmin(admin.ModelAdmin):
+    list_display, search_fields = get_model_fields(County)
+
+
+@admin.register(SubCounty)
+class SubCountyAdmin(admin.ModelAdmin):
+    list_display, search_fields = get_model_fields(SubCounty)
+
+
+@admin.register(Ward)
+class WardAdmin(admin.ModelAdmin):
+    list_display, search_fields = get_model_fields(Ward)
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display, search_fields = get_model_fields(Location)
+
+
+@admin.register(BusinessCategory)
+class BusinessCategoryAdmin(admin.ModelAdmin):
+    list_display, search_fields = get_model_fields(BusinessCategory)
+
+
+@admin.register(Business)
+class BusinessAdmin(admin.ModelAdmin):
+    list_display, search_fields = get_model_fields(Business)
+    list_display += ['age']  # Add the 'age' property to the list display
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display, search_fields = get_model_fields(Customer)
--- src/customer/models.py
+++ src/customer/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.utils import timezone
+from src.utils.models import TimeStamped
 
-# Create your models here.
+
+class County(TimeStamped):
+    name = models.CharField(max_length=100)
+
+
+class SubCounty(TimeStamped):
+    name = models.CharField(max_length=100)
+    county = models.ForeignKey(County, on_delete=models.CASCADE, related_name='sub_counties')
+
+
+class Ward(TimeStamped):
+    name = models.CharField(max_length=100)
+    sub_county = models.ForeignKey(SubCounty, on_delete=models.CASCADE, related_name='wards')
+
+
+class Location(models.Model):
+    building_name = models.CharField(max_length=100)
+    floor = models.CharField(max_length=255, blank=True, null=True)
+    ward = models.ForeignKey(Ward, on_delete=models.CASCADE, related_name='locations')
+
+
+class BusinessCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Business(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(BusinessCategory, on_delete=models.CASCADE, related_name='category_businesses')
+    registration_date = models.DateField()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='location_businesses')
+
+    @property
+    def age(self):
+        today = timezone.now().date()
+        return (today - self.registration_date).days // 365
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField()
+    date_of_birth = models.DateField()
+    nationality = models.CharField(max_length=100)
+    business = models.ForeignKey(Business, on_delete=models.CASCADE, related_name='customers')
