cd264b2b324c9201f6a398ce097a91354f2f162d
BrianMwevi <mwevibrian@gmail.com>
2024-04-29 17:13:18 +0300

    fix:add roles check

--- src/authorization/permissions.py
+++ src/authorization/permissions.py
@@ -2,6 +2,6 @@ class CanManageUserRoles(BasePermission):
 
     def has_permission(self, request, view):
         if request.method not in SAFE_METHODS:
-            return UserRolesService.check_roles(user=request.user, roles=[JAMBOPAY_ADMIN, KAA_ADMIN, ZONE_ADMIN])
+            return UserRolesService.check_roles(user=request.user, roles=['JAMBOPAY_ADMIN', 'CUSTOMER_MANAGEMENT'])
         return True
 
